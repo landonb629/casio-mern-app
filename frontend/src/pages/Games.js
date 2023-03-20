e2e2a6eef35a931110d463cf41f9e45e5f103051
Landon Babay <landon.babay@outlook.com>
2023-03-20 16:50:23 -0400

    working on the games page

--- frontend/src/pages/Games.js
+++ frontend/src/pages/Games.js
@@ -2,8 +2,53 @@ import React from "react";
 import {useState, useEffect} from 'react'
 
 
+
+
 const Games = () => { 
-    return <div>Games</div>
+    const [games, setGames] = useState([])
+    useEffect(()=> { 
+      const getGames = async () => {
+          try { 
+            const url = "http://localhost:3007/api/v1/games/"
+            const request = await fetch(url, {
+            method: "GET",
+            credentials: "include",
+            headers: { 
+                "Content-Type": "application/json"
+            }
+            })
+            const reqGames = await request.json()
+            console.log(reqGames)
+            setGames(reqGames.game);
+          } catch(error) { 
+           console.log(error) 
+          }
+        }
+        getGames()
+    },[])
+
+    return(
+       <section>
+           <div>
+               <h2>Games available</h2>
+           </div>
+           <div>    
+           {
+            games.map((game)=>{
+             return(
+                <div key={game._id} style={{display: 'inline-flex', alignItems: 'center', border: 'solid', padding: 10, margin: 10}}>
+                  <h4 style={{margin: 10}}>{game.name}</h4>
+                  <p style={{margin: 10}}> cost: {game.cost}</p>
+                  <div>
+                    <button>play</button>
+                  </div>
+                </div>
+             ) 
+            })
+            }
+           </div>
+       </section>
+    )
 }
 
 export default Games
