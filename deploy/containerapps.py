8a8a70a0457e2810be2f51068979950d8b4215e2
Landon Babay <landon.babay@outlook.com>
2023-05-12 10:28:07 -0400

    deploying some demo environments, and adding python SDK code

---
+++ deploy/containerapps.py
@@ -0,0 +1,48 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.appcontainers import ContainerAppsAPIClient 
+import os
+
+container_app_config = { 
+    "location": "East US",
+    "properties": {
+        "configuration": {
+            "ingress": {
+                "external": "true",
+                "targetPort": 80,
+                "allowInsecure": "false",
+            }
+        },
+    "environmentId": "",
+    "template": {
+        "containers": [ 
+            {
+                "image": "casinomernregistry.azurecr.io/backend:v1.0",
+                "name": "backend-casino"
+            }
+        ]
+    },
+    "scale": { 
+        "maxReplicas": 5,
+        "minReplicas": 1
+    },
+  },
+}
+
+sub_id = os.getenv("AZ_SUB_ID")
+
+if sub_id == 'None': 
+    print('no subscription id found')
+
+client = ContainerAppsAPIClient(credential=DefaultAzureCredential(), subscription_id=sub_id)
+
+client.managed_environments.begin_create_or_update(
+    resource_group_name="testing-py-sdk",
+    environment_name="test-env",
+    
+)
+
+client.container_apps.begin_create_or_update(
+    resource_group_name="testing-py-sdk",
+    container_app_name="testing-py-app",
+    container_app_envelope=container_app_config
+)
